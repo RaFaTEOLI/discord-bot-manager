2a4b79a72f20e6a146c019ab64678b8d40fce5eb
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2021-07-09 12:38:43 -0300

    Updated application port

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -14,6 +14,6 @@ app.get('/', (request: Request, response: Response) => {
   return response.send({ message: 'Hello World' });
 });
 
-app.listen(3333, () => {
-  console.log('✅  Server running on port 3333...');
+app.listen(process.env.PORT, () => {
+  console.log(`✅  Server running on port ${process.env.PORT}...`);
 });
